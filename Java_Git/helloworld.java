3448a5678ed482955397669dcb3424814d4ada3f
lan2405 <2405349439@qq.com>
2022-02-14 21:18:12 +0800

    0.1

--- Java_Git/helloworld.java
+++ Java_Git/helloworld.java
@@ -1,9 +1,9 @@
 package Java_Git;
 
-/**
- * 扫描仪类，创建扫描仪类对象
- * 扫描仪类对象拥有获取键盘中得到的字符，并指向其他关键字的方法 
- * */
+/*
+  扫描仪类，创建扫描仪类对象
+  扫描仪类对象拥有获取键盘中得到的字符，并指向其他关键字的方法
+  */
 import java.util.Scanner;
 
 
@@ -13,19 +13,19 @@ public class helloworld {
     static int args2;
     /*一个java允许重载多个main方法（主方法） */
     public static void main(String[] args) {
-        args2=Intinput();
-        args1=Stringinput();
+        args2=Int_input();
+        args1=String_input();
         Fishermen a=new Fishermen(args1,args2);
         a.show();
     }
-    public static int Intinput(){
-        int a=0;
+    public static int Int_input(){
+        int a;
         Scanner input=new Scanner(System.in);
         a=input.nextInt();
         input.close();
         return a;
     }
-    public static long Stringinput(){
+    public static long String_input(){
         long a;
         Scanner input=new Scanner(System.in);
         a=input.nextLong();
@@ -44,7 +44,7 @@ class Fishermen{
         gender=a;age=b;
     }
     public void show(){
-        System.out.printf("这个%-s渔民,%d岁",gender);
+        System.out.printf("这个%s渔民,%d岁",gender,age);
     }
     public void work(){
 
