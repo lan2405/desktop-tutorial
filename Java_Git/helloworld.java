163c88c9913e21ed410e73d2baa42865799a8812
lan2405 <2405349439@qq.com>
2022-02-14 20:45:34 +0800

    0.1

--- Java_Git/helloworld.java
+++ Java_Git/helloworld.java
@@ -1,24 +1,34 @@
 package Java_Git;
 
 /**
- * 扫描仪类，获取键盘中得到的字符，并指向其他关键字 
+ * 扫描仪类，创建扫描仪类对象
+ * 扫描仪类对象拥有获取键盘中得到的字符，并指向其他关键字的方法 
  * */
 import java.util.Scanner;
 
 
 public class helloworld {/*一个java文件只能有一个public类 */
-    int args1,args2;
+    static long args1;
+    static int args2;
     public static void main(String[] args) {
-        int i=one();
-        System.out.println(i);
+        args2=Intinput();
+        args1=Stringinput();
+        Fishermen a=new Fishermen(args1,args2);
+        a.show();
     }
-    public static int one(){
+    public static int Intinput(){
         int a=0;
         Scanner input=new Scanner(System.in);
         a=input.nextInt();
         input.close();
         return a;
-
+    }
+    public static long Stringinput(){
+        long a;
+        Scanner input=new Scanner(System.in);
+        a=input.nextLong();
+        input.close();
+        return a;
     }
 
 }
@@ -26,7 +36,17 @@ public class helloworld {/*一个java文件只能有一个public类 */
  * 渔民类 
  *  */
 class Fishermen{
-    
+    long gender;
+    int age;
+    Fishermen(Long a,int b){
+        gender=a;age=b;
+    }
+    public void show(){
+        System.out.printf("这个%-s渔民,%d岁",gender);
+    }
+    public void work(){
+
+    }
 }
 /**鱼竿类 */
 class Fishing_rod{
