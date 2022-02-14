9787218d19da650dfc561aea1d6ac72337b68be5
lan2405 <2405349439@qq.com>
2022-02-14 08:51:16 +0800

    0.1

--- Java_Git/helloworld.java
+++ Java_Git/helloworld.java
@@ -1,8 +1,38 @@
 package Java_Git;
 
+/**
+ * 扫描仪类，获取键盘中得到的字符，并指向其他关键字 
+ * */
+import java.util.Scanner;
 
-public class helloworld {
+
+public class helloworld {/*一个java文件只能有一个public类 */
+    int args1,args2;
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        int i=one();
+        System.out.println(i);
     }
+    public static int one(){
+        int a=0;
+        Scanner input=new Scanner(System.in);
+        a=input.nextInt();
+        input.close();
+        return a;
+
+    }
+
+}
+/**
+ * 渔民类 
+ *  */
+class Fishermen{
+    
+}
+/**鱼竿类 */
+class Fishing_rod{
+
+}
+/**鱼类 */
+class Fish{
+
 }
